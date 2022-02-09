c10992510467d6db574cbf64759fe35e12b51b50
jayedapu <62106555+jayedapu@users.noreply.github.com>
2022-02-10 00:33:32 +0600

    Updates

---
+++ precache-manifest.2bbd919b83fe9ebc6526dc3b2f672158.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001cdf3abc28c76525eab938559ea441",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "79b41b48b5947944c446",
+    "url": "/portfolio/static/css/main.b3a816f4.chunk.css"
+  },
+  {
+    "revision": "9d2823e139068ae8bb0b",
+    "url": "/portfolio/static/js/2.9e10806b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.9e10806b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b41b48b5947944c446",
+    "url": "/portfolio/static/js/main.58d49234.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0685327bad5eeb03ebda59c4e5b19ffc",
+    "url": "/portfolio/static/media/COVID.0685327b.jpg"
+  },
+  {
+    "revision": "b76d63af45090d62aa7a8d767c3d0386",
+    "url": "/portfolio/static/media/Dev_talks.b76d63af.jpg"
+  },
+  {
+    "revision": "f5711eaa4996e1483f221e7c57cb7905",
+    "url": "/portfolio/static/media/algo.f5711eaa.svg"
+  },
+  {
+    "revision": "63f68ccc85cc2ab7da913353c08cccaf",
+    "url": "/portfolio/static/media/api.63f68ccc.svg"
+  },
+  {
+    "revision": "b1fb38410219279a306a759e265304a5",
+    "url": "/portfolio/static/media/apu.b1fb3841.JPG"
+  },
+  {
+    "revision": "a42040d5c68d4d3fed9b27878d2fcca5",
+    "url": "/portfolio/static/media/backend.a42040d5.svg"
+  },
+  {
+    "revision": "8931471bc5121c5f5f684e080ef9fe8b",
+    "url": "/portfolio/static/media/blinking.8931471b.JPG"
+  },
+  {
+    "revision": "6b5433ec6558cf134d5f6c33b724bac2",
+    "url": "/portfolio/static/media/chatapp.6b5433ec.JPG"
+  },
+  {
+    "revision": "f69a82f60f211b15f91ad1897d867b69",
+    "url": "/portfolio/static/media/color_classification.f69a82f6.jpg"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/portfolio/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "0a27bbff6aa4dfb3bf8606c2fba69fd6",
+    "url": "/portfolio/static/media/facebook.0a27bbff.svg"
+  },
+  {
+    "revision": "cbc38ef18c5a6ecdc0ace14b23e84380",
+    "url": "/portfolio/static/media/github.cbc38ef1.svg"
+  },
+  {
+    "revision": "4ba98a823d931f922f94ff80977e61b0",
+    "url": "/portfolio/static/media/hardware_store.4ba98a82.jpg"
+  },
+  {
+    "revision": "d08bce0dcb53ad1ef20b0cae18977145",
+    "url": "/portfolio/static/media/instagram.d08bce0d.svg"
+  },
+  {
+    "revision": "82f6dd4ae54df664f0154e3465f7e2e7",
+    "url": "/portfolio/static/media/new_year.82f6dd4a.jpg"
+  },
+  {
+    "revision": "64c56c72135ba0b4ddf8da186182b723",
+    "url": "/portfolio/static/media/pin.64c56c72.svg"
+  },
+  {
+    "revision": "45b48090de6a5fe37055f98339319dc4",
+    "url": "/portfolio/static/media/portfolio.45b48090.JPG"
+  },
+  {
+    "revision": "b5fac2be7a6a8edc808f550a24217391",
+    "url": "/portfolio/static/media/puzzle.b5fac2be.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/portfolio/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/portfolio/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "1464053c658e62318c689e56c4f1643c",
+    "url": "/portfolio/static/media/repair.1464053c.svg"
+  },
+  {
+    "revision": "2942bfabb3d05332b66eb128e0842cff",
+    "url": "/portfolio/static/media/resume.2942bfab.pdf"
+  },
+  {
+    "revision": "4a04f9f0dd3f59df96497cc7c3021550",
+    "url": "/portfolio/static/media/tie.4a04f9f0.svg"
+  },
+  {
+    "revision": "2185e423b1c8b70c671e1c927b95d4cd",
+    "url": "/portfolio/static/media/tweetme.2185e423.PNG"
+  }
+]);
